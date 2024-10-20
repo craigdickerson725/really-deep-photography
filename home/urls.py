5f23b7a50d81e01f0bdea4809fb1eb3553a9c448
Craig Dickerson <craigdickerson@hotmail.com>
2024-10-20 08:43:00 +0000

    Add gallery and about templates

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import index, GalleryView, AboutView, ContactView
+from allauth.account.views import LoginView
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', index, name='home'),
+    path('gallery/', GalleryView.as_view(), name='gallery'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('login/', LoginView.as_view(), name='account_login'),  # Use Allauth's login view
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+# Home view
 def index(request):
-    """ A view to return the index page """
-    
     return render(request, 'home/index.html')
+
+# Gallery view
+class GalleryView(TemplateView):
+    template_name = 'home/gallery.html'
+
+# About view
+class AboutView(TemplateView):
+    template_name = 'home/about.html'
+
+# Contact view
+class ContactView(TemplateView):
+    template_name = 'home/contact.html'
