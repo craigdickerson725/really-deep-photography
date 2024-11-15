80c2c5eb61a1757210281c41cd5f5b5fdacdd709
Craig Dickerson <craigdickerson@hotmail.com>
2024-11-15 09:03:46 +0000

    Refactor main manage python file for pep8 validation

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'really_deep_photography.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'really_deep_photography.settings')  # noqa
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
