370f808aef5ec04304adee5003f90d9a79814d10
Craig Dickerson <craigdickerson@hotmail.com>
2024-12-22 20:46:40 +0000

    Correct settings database error

--- really_deep_photography/settings.py
+++ really_deep_photography/settings.py
@@ -12,9 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG", False)
-
-DEBUG = TRUE
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = [
     '8000-craigdicker-reallydeepp-lvdcmwz1rbr.ws.codeinstitute-ide.net',
