e4e7aeaf0bc44445ce1389d15dfc84e604ba58c4
Craig Dickerson <craigdickerson@hotmail.com>
2024-12-22 20:44:00 +0000

    Troubleshoot server error for deployed version

--- really_deep_photography/settings.py
+++ really_deep_photography/settings.py
@@ -12,7 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+# DEBUG = os.environ.get("DEBUG", False)
+
+DEBUG = TRUE
 
 ALLOWED_HOSTS = [
     '8000-craigdicker-reallydeepp-lvdcmwz1rbr.ws.codeinstitute-ide.net',
